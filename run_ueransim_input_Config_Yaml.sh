fdb7e3b47ae0babd0f56c0af9f9b38eddf99b04a
TRAN DINH HIEU <trandinhhieu1989@gmail.com>
2023-08-30 21:10:48 +0200

    Update run_ueransim_input_Config_Yaml.sh

--- run_ueransim_input_Config_Yaml.sh
+++ run_ueransim_input_Config_Yaml.sh
@@ -34,6 +34,5 @@ source /media/hieu/E/Hieu/Dockerfree5gc_github/Pymongo/venv/bin/activate
 # Add 2nd UE's information into MongoDB
 python3 /media/hieu/E/Hieu/Dockerfree5gc_github/Pymongo/Add_UE_general.py $imsi_1 $imsi_2 $key_1 $key_2
 
-docker run --privileged --network hieunetwork --ip 172.18.0.14 --name ue_hieu -dit ue:1.0.0  172.18.0.13 imsi-208930000000001 8baf473f2f8fd09487cccbd7097c6862
-docker run --privileged --network hieunetwork --ip 172.18.0.15 --name ue1_hieu -dit ue:1.0.0 172.18.0.13 imsi-208930000000002 8baf473f2f8fd09487cccbd7097c6863
-
+docker run --privileged --network hieunetwork --ip 172.18.0.14 --name ue_hieu -dit ue:1.0.0  172.18.0.13 $imsi_1 $key_1
+docker run --privileged --network hieunetwork --ip 172.18.0.15 --name ue1_hieu -dit ue:1.0.0 172.18.0.13 $imsi_2 $key_2
